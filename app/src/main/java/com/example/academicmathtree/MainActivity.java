af5ffcf0bd403738c2c0801064ec813ddaf0bdfc
RaimundoNeto7 <raimundoneto.rn17@gmail.com>
2020-02-24 16:15:27 -0400

    Showing the initial list of academics

--- app/src/main/java/com/example/academicmathtree/MainActivity.java
+++ app/src/main/java/com/example/academicmathtree/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.academicmathtree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listViewAcademics;
+    private ArrayList<String>  listAcademicsRandom;
+    private ArrayAdapter<String> adapterListAcademics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listViewAcademics = findViewById(R.id.listview_academics);
+        initializeListAcademics();
+        adapterListAcademics = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, listAcademicsRandom);
+        listViewAcademics.setAdapter(adapterListAcademics);
+
+        populateListAcademics(adapterListAcademics);
+    }
+
+    private void initializeListAcademics(){
+        listAcademicsRandom = new ArrayList<>();
+    }
+
+    private void populateListAcademics(ArrayAdapter adapterListAcademics){
+        listAcademicsRandom.add("Beatriz Albuquerque");
+        listAcademicsRandom.add("Ana Albuquerque");
+        listAcademicsRandom.add("Raimundo Neto");
+        listAcademicsRandom.add("Carol Alguma Coisa");
+
+        adapterListAcademics.notifyDataSetChanged();
     }
 }
