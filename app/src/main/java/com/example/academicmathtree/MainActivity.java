ef9602970666c4633aee43999f8048b56624198f
RaimundoNeto7 <raimundoneto.rn17@gmail.com>
2020-02-24 15:59:52 -0400

    Creating a listview to show the academics

--- app/src/main/java/com/example/academicmathtree/MainActivity.java
+++ app/src/main/java/com/example/academicmathtree/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.academicmathtree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewAcademics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listViewAcademics = findViewById(R.id.listview_academics);
     }
 }
