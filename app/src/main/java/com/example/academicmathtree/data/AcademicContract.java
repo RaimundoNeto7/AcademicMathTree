429da2494ec3d7bcc556450529849060251c7535
Raimundo Neto <raimundoneto.rn17@gmail.com>
2020-02-26 17:25:10 -0400

    Creating the academic contract class

---
+++ app/src/main/java/com/example/academicmathtree/data/AcademicContract.java
@@ -0,0 +1,9 @@
+package com.example.academicmathtree.data;
+
+import android.net.Uri;
+
+public class AcademicContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.academictree";
+    public static final Uri BASE_CONTENT_URI = Uri.parse(CONTENT_AUTHORITY);
+}
