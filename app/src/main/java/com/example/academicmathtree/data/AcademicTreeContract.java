a71b661c2e63db00bc936e65da9c462187fe51be
Raimundo Neto <raimundoneto.rn17@gmail.com>
2020-02-26 17:38:28 -0400

    Changing the name Academic to AcademicTree

--- app/src/main/java/com/example/academicmathtree/data/AcademicTreeContract.java
+++ app/src/main/java/com/example/academicmathtree/data/AcademicTreeContract.java
@@ -3,7 +3,7 @@ package com.example.academicmathtree.data;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
-public class AcademicContract {
+public class AcademicTreeContract {
 
     public static final String CONTENT_AUTHORITY = "com.example.academictree";
     public static final Uri BASE_CONTENT_URI = Uri.parse(CONTENT_AUTHORITY);
