5084defa00a48721327990a0c704686ca6fb8c53
RaimundoNeto7 <raimundoneto.rn17@gmail.com>
2020-02-26 20:56:38 -0400

    Inserting a UriMatcher with 2 options: CODE_ACADEMIC and CODE_ACADEMIC_BY_ID

--- app/src/main/java/com/example/academicmathtree/data/AcademicTreeProvider.java
+++ app/src/main/java/com/example/academicmathtree/data/AcademicTreeProvider.java
@@ -2,6 +2,7 @@ package com.example.academicmathtree.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -9,6 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class AcademicTreeProvider extends ContentProvider {
+
+    public static final int CODE_ACADEMIC = 100;
+    public static final int CODE_ACADEMIC_BY_ID = 101;
+
+    private static final UriMatcher matcher = buildUriMatcher();
+    private AcademicTreeDbHelper dbHelper;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = AcademicTreeContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, AcademicTreeContract.PATH_ACADEMIC, CODE_ACADEMIC);
+        matcher.addURI(authority, AcademicTreeContract.PATH_ACADEMIC + "/#", CODE_ACADEMIC_BY_ID);
+        
+        return matcher;
+    }
+
     @Override
     public boolean onCreate() {
         return false;
