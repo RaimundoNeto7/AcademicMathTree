0019dcf0daf4813e698f174f1fb65e477bb131ce
Raimundo Neto <raimundoneto.rn17@gmail.com>
2020-02-26 21:36:50 -0400

    Creating a method to inserting academics data

---
+++ app/src/main/java/com/example/academicmathtree/data/AcademicStaticData.java
@@ -0,0 +1,27 @@
+package com.example.academicmathtree.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.example.academicmathtree.data.AcademicTreeContract.AcademicEntry;
+import com.example.academicmathtree.model.AcademicModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.academicmathtree.data.AcademicContentValue.getAcademicValues;
+
+public class AcademicStaticData {
+    //public AcademicModel(String uid, String name, String university, String expertise, String job, String description, Boolean isRoot) {
+    public static void insertStaticData(Context context){
+        List<ContentValues> contentValues = new ArrayList<>();
+
+        ContentValues values = getAcademicValues(new AcademicModel(1, "Raimundo Neto", "UFAM", "Science Computer", "Software Engineer", "Handsome", true));
+        contentValues.add(values);
+
+        context.getContentResolver().bulkInsert(
+                AcademicEntry.CONTENT_URI,
+                contentValues.toArray(new ContentValues[contentValues.size()])
+        );
+    }
+}
