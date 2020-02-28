210fcdb32463fc157ea79138f89b8a9a2dff5198
Raimundo Neto <raimundoneto.rn17@gmail.com>
2020-02-27 21:33:51 -0400

    Inserting data in AcademicStaticData

--- app/src/main/java/com/example/academicmathtree/data/AcademicStaticData.java
+++ app/src/main/java/com/example/academicmathtree/data/AcademicStaticData.java
@@ -20,11 +20,15 @@ public class AcademicStaticData {
 
         ContentValues values = getAcademicValues(new AcademicModel(1, "Raimundo Neto", "UFAM", "Science Computer", "Software Engineer", "Handsome", true));
         contentValues.add(values);
+        ContentValues values2 = getAcademicValues(new AcademicModel(2, "RN", "UFAM", "Science Computer", "Software Engineer", "Handsome", true));
+        contentValues.add(values2);
+        ContentValues values3 = getAcademicValues(new AcademicModel(3, "Rai2mundo Neto", "UFAM", "Science Computer", "Software Engineer", "Handsome", true));
+        contentValues.add(values3);
         int rowsInserted = context.getContentResolver().bulkInsert(
                 AcademicEntry.CONTENT_URI,
-                contentValues.toArray(new ContentValues[contentValues.size()])
+                contentValues.toArray(new ContentValues[3])
         );
 
-        Log.e("AcademicProvider", "Rows inserted: " + rowsInserted);
+        Log.e("AcademicProvider", "Academics inserted: " + rowsInserted);
     }
 }
